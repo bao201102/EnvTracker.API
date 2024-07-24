44d6c83f55533d66f09e15800858346af7b73fb7
bao201102 <bao201102@gmail.com>
2024-07-24 21:12:07 +0700

    (...) add controller permission

---
+++ EnvTracker.API/Controllers/USR/PermissionsController.cs
@@ -0,0 +1,36 @@
+﻿using EnvTracker.API.Common;
+using EnvTracker.Application.DTOs.Response.USR.Permission;
+using EnvTracker.Application.DTOs.Response.USR.Role;
+using EnvTracker.Application.Services.Interfaces.USR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnvTracker.API.Controllers.USR
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PermissionsController : BaseController
+    {
+        private readonly IPermissionService _service;
+
+        public PermissionsController(IPermissionService service)
+        {
+            _service = service;
+        }
+
+
+        /// <summary>
+        /// Get all Permission
+        /// </summary>
+        /// 2024-07-24 - BaoNN
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<PermissionRes>))]
+        [Authorize]
+        public async Task<IActionResult> List()
+        {
+            var result = await _service.List();
+            return ApiOK(result);
+        }
+    }
+}
